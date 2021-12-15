d7f5cbeaae20e294176cc2b968187e6bf63b593b
KathyVelito <85207829+KathyVelito@users.noreply.github.com>
2021-12-15 17:31:43 -0500

    video

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    
+    extend: {
+      colors : {
+        clank : {
+          100: '#212121',
+          200: '#191919',
+          300: '#323232',
+          400: '#333333',
+          500: '#282828'
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  }
 }
